701dd52cf15a61eba6d68626a808365991d23956
Pawel Makulec <s33218@pjwstk.edu.pl>
2026-03-18 18:37:11 +0100

    obsługa błędów

--- Program.cs
+++ Program.cs
@@ -11,3 +11,8 @@ int a = int.Parse(Console.ReadLine());
 int b = a * a;
 Console.WriteLine("Oto kwadrat z twojej liczby= " + b);
 
+if (b % 2 != 0)
+{
+    throw new ArgumentException("Kwadrat liczby nie jest parzysty");
+}
+
